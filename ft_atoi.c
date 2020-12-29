79dc41ff1e1c57f236cac05a4f3ff3ab1ba1889a
yujung <modo000127@gmail.com>
2020-12-29 13:13:05 +0900

    part1_deep0

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,12 +1,20 @@
 int ft_atoi(const char *str)
 {
-	int ans;
+	unsigned int ans;
+	int sign;
 	char *s;
 
 	ans = 0;
+	sign = 1;
 	s = (char *)str;
 	while (*s == ' ')
 		s++;
+	if (*s == '-' || *s == '+')
+	{
+		s++;
+		if (*s == '-')
+			sign = -1;
+	}
 	while (*s)
 	{
 		if ('0' <= *s && *s <= '9')
@@ -15,6 +23,6 @@ int ft_atoi(const char *str)
 			break;
 		s++;
 	}
-	return (ans);
+	return (sign * ans);
 }
 
