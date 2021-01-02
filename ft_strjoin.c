2e697f83e8b60b2dcd4c7bac3896a42a53dfea93
Yunjoo Jung <yujung@c3r6s5.42seoul.kr>
2021-01-02 22:02:02 +0900

    part2_2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,19 +4,16 @@
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	char *str;
-	//int i;
+	int i;
 
-	//i = 0;
+	i = 0;
 	if (!(str = malloc(sizeof(char))))
 		return (NULL);
-	
-	ft_strlcpy(str,s1,ft_strlen(s1));
-	ft_strlcpy(str,s2,ft_strlen(s2));
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
 	return (str);
 }
 
-int main()
-{
-	printf("%s\n", ft_strjoin("abc","def"));
-	return 0;
-}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include <stdio.h>
 
 char *ft_strtrim(char const *s1, char const *set)
 {
@@ -8,11 +9,66 @@ char *ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	if (!(str = malloc(sizeof(char))))
 		return (NULL);
-	while (set[i] || !(ft_strchr(s1, set[i])))
-		i++;
-	while (set[i] || !(ft_strrchr(s1, set[i])))
-		i--;
-		
+	//while (set[i] || !(str = ft_strchr(s1, set[i])))
+	//{
+	//	str = ft_strchr(s1, set[i]);
+	//	i++;
+	//}
+	str = ft_strchr(s1, set[i]);
 	str[i] = '\0';
 	return (str);
+}
+
+
+char *ft_strchr(const char *s, int c)
+{
+	char *d;
+	   
+	d = (char *)s;
+	while (*d != c)
+	{
+		if(*d == '\0')
+			return (0);
+		d++;
+	}
+	return (d);
+}
+
+char *ft_strrchr(const char *s, int c)
+{
+	char *str;
+	char *an;
+
+	str = (char *)s;
+	an = NULL;
+	while (*str)
+		    {
+				        if (*str == (char)c)
+							            an = str;
+						        str++;
+								    }
+	if (*str == c)
+		return (str);
+	return (an);
+}
+
+int main()
+{
+	char a[] = "havana";
+	char set[] = "abc";
+	char *str;
+	int i = 0;
+
+	while (set[i] || str == NULL)
+	{
+		str = ft_strchr(a, set[i]);
+		i++;
+	}
+	while (i > 0 || str == NULL)
+	{
+		str = ft_strchr(a,set[i]);
+		i--;
+	}
+	printf("%s\n",str);
+	return 0;
 }
