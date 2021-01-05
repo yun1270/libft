9793b4c3648b31434aa202fe235981bfd2b2bca2
Yunjoo Jung <yujung@c1r1s2.42seoul.kr>
2021-01-05 18:52:52 +0900

    part2_ok

--- ft_split.c
+++ ft_split.c
@@ -1,21 +1,75 @@
 #include "libft.h"
 
-char **ft_split(char const *s, char c)
+int count(char const *s, char c)
 {
-	char **str;
+    int i;
+    int n;
+    int st;
 
-	if (!(str = malloc(sizeof(char))))
-		return (NULL);
-	str[0] = s;
-	str[1][0] = c;
-	return (str);
+    n = 0;
+    i = 0;
+    st = 0;
+    while (s[i])
+    {
+        if (s[i] == c)
+            st = 0;
+        else if (st == 0)
+        {
+            st = 1;
+            n++;
+        }
+        i++;
+    }
+    return (n);
 }
 
-int main()
+int count_c(char const *s, char c, int i)
 {
-	char s[] = "hello my frined";
-	char c = ' ';
+    int n;
 
+    n = 0;
+    while (s[i] != c && s[i++])
+        n++;
+    return (n);
+}
 
-	return 0;
+char **get_str(char **str, char const *s, char c, int n)
+{
+    int i;
+    int j;
+    int k;
+
+    i = 0;
+	j = 0;
+    while (s[i] && j < n)
+    {
+        k = 0;
+        while (s[i] == c)
+            i++;
+        if (!(str[j] = malloc(sizeof(char) * count_c(s, c, i) + 1)))
+        {
+            while (j > 0)
+                free((void *)str[j--]);
+            free(str);
+            return (NULL);
+        }
+        while (s[i] != c && s[i])
+            str[j][k++] = s[i++];
+        str[j++][k] = '\0';
+    }
+    str[j] = 0;
+    return (str);
+}
+
+char **ft_split(char const *s, char c)
+{
+    char **str;
+    int n;
+
+    if (s == NULL)
+        return (NULL);
+    n = count(s, c);
+    if (!(str = malloc(sizeof(char *) * (n + 1))))
+        return (NULL);
+    return (get_str(str, s, c, n));
 }
